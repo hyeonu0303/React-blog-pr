709aaf2651b2bee6770d002484e3d74aa095ba32
Parkhyeonwoo <hyeonu030334@gmail.com>
2023-07-11 10:11:52 +0900

    버튼 기능구현 state변경하는법

--- src/App.js
+++ src/App.js
@@ -1,24 +1,36 @@
-import logo from './logo.svg';
+/*eslint-disable */
+
+import { useState } from 'react';
 import './App.css';
 
 function App() {
+  
+  //a는 state보관자료남음 b는 state변경도와줌 Destructuring문법
+  //변동시 자동으로 반영되게 만들고싶으면 state쓰기
+  let [글제목,set글제목]=useState(['남자 코트 추천','강남 우동맛집','파이썬독학'])
+  let logo = 'HY.BLOG'
+  let [date,setdate] = useState(['7월11일 발행','7월 10일발행','7월 12일발행'])
+  let [따봉, 따봉변경] = useState(0);
+  //state변경 등호금지
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <div className="black-nav">
+        <h3 style={{margin:'0px'}}>{logo}</h3>
+      </div>
+      <div className='list'>
+        <h4>{글제목[0]}<span onClick={()=>{따봉변경(따봉+1)}}>👍</span>{따봉}</h4>
+        <p>{date[0]}</p> 
+      </div>
+      <div className='list'>
+        <h4>{글제목[1]}</h4>
+        <p>{date[1]}</p> 
+      </div>
+      <div className='list'>
+        <h4>{글제목[2]}</h4>
+        <p>{date[2]}</p> 
+      </div>
+    </div> 
   );
 }
 
